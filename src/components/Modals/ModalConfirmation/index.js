6990598a2fbbbdf3db971a141087eee2b59a3d62
Isaque Vilson <ivbc.eng17@uea.edu.br>
2020-07-12 16:21:15 -0400

    fix name app and modal

--- src/components/Modals/ModalConfirmation/index.js
+++ src/components/Modals/ModalConfirmation/index.js
@@ -22,14 +22,6 @@ import {
   ButtonConfirmContainer,
 } from './styles';
 
-const deviceWidth = Dimensions.get('window').width;
-const deviceHeight =
-  Platform.OS === 'ios'
-    ? Dimensions.get('window').height
-    : require('react-native-extra-dimensions-android').get(
-        'REAL_WINDOW_HEIGHT'
-      );
-
 function ModalConfirmation({ isOpen, close, guests: _guests, confirmGuests }) {
   const [guests, setGuests] = useState(_guests);
 
@@ -44,11 +36,7 @@ function ModalConfirmation({ isOpen, close, guests: _guests, confirmGuests }) {
     close();
   };
   return (
-    <Modal
-      isVisible={isOpen}
-      deviceWidth={deviceWidth}
-      deviceHeight={deviceHeight}
-    >
+    <Modal isVisible={isOpen}>
       <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         <StatusBar backgroundColor="rgba(0,0,0,0.7)" />
         <ContainerModal>
